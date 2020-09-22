5afc35875f1c0aab28971b1ff38c27dcaf97057f
KurniaMulyaR <63498139+KurniaMulyaR@users.noreply.github.com>
2020-09-22 12:11:20 +0700

    instal cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require("express-session");
 const flash = require("connect-flash");
-const cors = require("cors")
+const cors = require("cors");
 // import mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://Kurniamr:bwamern@kurniadb-xyvmn.mongodb.net/db_staycation?retryWrites=true&w=majority', {
